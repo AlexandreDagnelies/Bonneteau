abd448c290c143fa4a942dec906c64b7c4831619
Alexandre Dagnelies <alexandre.dagnelies@koben.fr>
2016-06-09 21:17:08 +0200

    Fixed bug poorly mixed cups, Adding new images

--- js/functions.js
+++ js/functions.js
@@ -29,7 +29,7 @@ function startModule(param)
  * Function called from the app when the scenario restart for the next customer
  * @param param context saved by the app
  */
-function startModule(param)
+function restartModule(param)
 {
     if (param)
         context = param;
@@ -48,9 +48,6 @@ if (!isMobile) {
     });
 }
 
-
-function reset() {
-}
 
 
 var showItem = function (randomInt) {
@@ -90,53 +87,25 @@ var verif = function (randomInt) {
     })
 }
 
+   
 var start = function () {
+    
     $('.button').fadeOut(1000, function() {
         $('.gobelets').fadeIn(100);
         randomInt = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
         showItem(randomInt);
-        verif(randomInt);});
+        verif(randomInt);
+        //empeche l'utilisateur de cliquer pendant l'action
+        $('body').append( "<div id='shield'></div>" );
+    });
   
 
 
+};
 
-}
-
-
-/*
 function swichPlaceRight(divRight, divMiddle, speed) {
-
-    var bezier_params = {
-        start: {x: 400, y: 70,angle: -400},
-        end: { x: 255, y: 100,  angle: 40, length: 0.2 }
-    }
-
-    var bezier_params2 = {
-        start: {x: 255, y: 100, angle: -400},
-        end: { x: 400,y: 70, angle: 40,length: 0.2}
-    }
-    
-    $(divRight).animate({path : new $.path.bezier(bezier_params)}, speed);
-    $(divMiddle).animate({path : new $.path.bezier(bezier_params2)}, speed);
-}
-
-function swichPlaceLeft(divLeft, divMiddle, speed) {
-
-    var bezier_params = {
-        start: {x: 100, y: 70,angle: -400},
-        end: { x: 250, y: 100,  angle: 40, length: 0.2 }
-    }
-
-    var bezier_params2 = {
-        start: {x: 250, y: 100, angle: -400},
-        end: { x: 100,y: 70, angle: 40,length: 0.2}
-    }
-    
-    $(divLeft).animate({path : new $.path.bezier(bezier_params)}, speed);
-    $(divMiddle).animate({path : new $.path.bezier(bezier_params2)}, speed);
-}*/
-function swichPlaceRight(divRight, divMiddle, speed) {
-
+    $(divMiddle).css({"z-index":"0"});
+    $(divRight).css({"z-index":"1"});
     var bezier_params = {
         start: {x: divRight.position().left, y: divRight.position().top, angle: -400},
         end: { x: divMiddle.position().left,y: divMiddle.position().top, angle: 40,length: 0.2}
@@ -152,7 +121,8 @@ function swichPlaceRight(divRight, divMiddle, speed) {
 }
 
 function swichPlaceLeft(divLeft, divMiddle, speed) {
-
+     $(divMiddle).css({"z-index":"1"});
+    $(divLeft).css({"z-index":"0"});
     var bezier_params = {
         start: {x: divLeft.position().left, y:  divLeft.position().top,angle: -400},
         end: { x: divMiddle.position().left, y: divMiddle.position().top,  angle: 40, length: 0.2 }
@@ -168,13 +138,13 @@ function swichPlaceLeft(divLeft, divMiddle, speed) {
     
 }
 var trick = 0;
-var vitesselumiere = 0;
+var SpeedIncrement = 0;
 function game(val, speed){
     console.log(trick);
-    console.log(vitesselumiere);
+    console.log(SpeedIncrement);
     //on recéupère la positon des 3 gobelets et on les places dans un tableau
     var cupArray = [$("#cupOne"), $("#cupTwo"), $("#cupThree")];
-    if(trick < 15){
+    if(trick < 16){
             //On trie les gobelets
         cupArray.sort(function(a, b) {
             return a.position().left - b.position().left;
@@ -185,40 +155,20 @@ function game(val, speed){
         //Mode Aléatoire
         val = Math.floor(Math.random() * 2)+1;
         if(val == 1){
-            swichPlaceLeft(leftCup, middleCup, (speed-vitesselumiere)); 
+            swichPlaceLeft(leftCup, middleCup, (speed-SpeedIncrement)); 
         }else{
-             swichPlaceRight(rightCup, middleCup, (speed-vitesselumiere));
+             swichPlaceRight(rightCup, middleCup, (speed-SpeedIncrement));
 
         }
     }else{
-       console.log('trick cest la fin');
+        //fin du jeu
        //On remet trick à zéro pour relancer
        trick = 0;
+       //On redonne accès au click à l'utilisateur
+        $('#shield').remove();
     }
-    
-    vitesselumiere += 30;
+        
+    SpeedIncrement += 30;
     trick++;
-}
-
-
-function play(){
-    game(1, 700);
-    
-    // game(1, 700, function() {alert('coucou');});
-    //game(2, 700, function() {console.log('coucou2');});
-    /*
-    var val = Math.floor(Math.random() * 2)+1;
-    game(val, 700, function() {
-            alert('Hello');
-        });
-    
-    for (var i = 0; i < 3; i++) {
-        val = (1+2)-val;
-        console.log(val);
-        game(val, 700, function() {
-            game(val, 700);
-        } );
-    }
-    */
 }
 
